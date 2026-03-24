ea0f03bf7391a45ec3ceada0d462354077deaf61
jiankn <jiankn@gmail.com>
2026-03-25 01:10:45 +0800

    Consolidate sitemap signals for googlebot

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,9 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://sunrisetime.co',
   adapter: cloudflare(),
-  integrations: [
-    sitemap({
-      changefreq: 'daily',
-      priority: 0.7,
-      lastmod: new Date(),
-      filter: (page) => {
-        const excludedPages = ['/404', '/privacy/', '/terms/', '/contact/', '/sitemap/'];
-        return !excludedPages.some((segment) => page.includes(segment));
-      },
-    }),
-  ],
   vite: {
     css: {
       devSourcemap: true,
